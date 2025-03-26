77e74bce41e4378508b6a0eee9fd87f9ce76f4ca
Abenezer Haymanot <143000984+abeni-csa@users.noreply.github.com>
2025-03-26 02:57:05 -0700

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,100 +1,37 @@
-use rand::Rng;
+use std::ops;
 
-struct Sheep {
-    naked: bool,
-    name: &'static str,
-}
-struct SheepQ {}
-struct CowQ {}
+struct Foo;
+struct Bar;
+#[derive(Debug)]
+struct FooBar;
 
-trait Animal {
-    // Associated function signature; `Self` refes to the implemetor type
-    fn new(name: &'static str) -> Self;
-    // Method signatures; these will return a string
-    fn name(&self) -> &'static str;
-    fn noise(&self) -> &'static str;
-    // these can provide defualt methond definitions
-    fn talk(&self) {
-        println!("{} says {}", self.name(), self.noise())
+#[derive(Debug)]
+struct BarFoo;
+// The `std::opp::Add` trait is used to sepcify the functionaltiey of `+`
+// Here, we make `Add<Bar>` - the trait for addition with RHS of type `Foo`
+// This block implements teh opration: Bar + Foo = BarFOo
+
+impl ops::Add<Bar> for Foo {
+    type Output = FooBar;
+    fn add(self, _rhs: Bar) -> Self::Output {
+        println!("> Bar.add(Foo) was Called");
+        FooBar
     }
 }
+// The `std::opp::Add` trait is used to sepcify the functionaltiey of `+`
+// Here, we make `Add<Bar>` - the trait for addition with RHS of type `Foo`
+// This block implements teh opration: Bar + Foo = BarFOo
 
-trait AnimalQ {
-    fn noise(&self) -> &'static str;
-}
-// Implemet the `AnimalQ` trait for `SheepQ`
-impl AnimalQ for SheepQ {
-    fn noise(&self) -> &'static str {
-        "baaaaaaaaaah!"
-    }
-}
-// Implemet the `AnimalQ` trait for `CowQ`
-impl AnimalQ for CowQ {
-    fn noise(&self) -> &'static str {
-        "mooooooooooh!"
-    }
-}
-// Return some Struct that implements Animal,
-// but we don't Know which one at compile time
-fn random_animal(random_number: i64) -> Box<dyn AnimalQ> {
-    if random_number < 5 {
-        Box::new(SheepQ {})
-    } else {
-        Box::new(CowQ {})
+impl ops::Add<Foo> for Bar {
+    type Output = BarFoo;
+    fn add(self, _rhs: Foo) -> Self::Output {
+        println!("> Bar.add(Foo) was Called");
+        BarFoo
     }
 }
 
-impl Sheep {
-    fn is_naked(&self) -> bool {
-        self.naked
-    }
-    fn shear(&mut self) {
-        if self.is_naked() {
-            // implementor methods can use implementor's trait methods.
-            println!("{} is alredy neked", self.name());
-        } else {
-            println!("{} gets a hairtuct!", self.name);
-            self.naked = true;
-        }
-    }
-}
-// implint Animal for Sheep
-impl Animal for Sheep {
-    // `Self` is implementor type: `Sheep`
-    fn new(in_name: &'static str) -> Sheep {
-        Sheep {
-            naked: false,
-            name: in_name,
-        }
-    }
-    fn name(&self) -> &'static str {
-        self.name
-    }
-    fn noise(&self) -> &'static str {
-        if self.is_naked() {
-            "baaaaaah?"
-        } else {
-            "beeeeeeeh!!"
-        }
-    }
-    // Defualt trait method can overridden
-    fn talk(&self) {
-        // for example we can add some quite contemplation.
-        println!("{} pouses briefly....{}", self.name, self.noise());
-    }
-}
 fn main() {
-    // type annotations is necessary in theis cause.
-    let mut dolly: Sheep = Animal::new("Dooly");
-    dolly.talk();
-    dolly.shear();
-    dolly.talk();
-    let rar_number = rand::rng().random_range(0..10);
-    let animal = random_animal(rar_number);
-    println!(
-        "You've randomly Chosen an animal, \
-        and it says {}
-        ",
-        animal.noise()
-    );
+    println!("Foo + Bar = {:?}", Foo + Bar);
+    println!("Bar + Foo = {:?}", Bar + Foo);
+    println!("Hello, world!");
 }
