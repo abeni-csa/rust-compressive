ebaed161236caa06144be777fac17c88b172320a
Abenezer Haymanot <143000984+abeni-csa@users.noreply.github.com>
2025-03-28 18:42:40 +0300

    error on lifetime

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,17 @@
-use std::ops;
-
-struct Foo;
-struct Bar;
-#[derive(Debug)]
-struct FooBar;
-
-#[derive(Debug)]
-struct BarFoo;
-// The `std::opp::Add` trait is used to sepcify the functionaltiey of `+`
-// Here, we make `Add<Bar>` - the trait for addition with RHS of type `Foo`
-// This block implements teh opration: Bar + Foo = BarFoo
-
-impl ops::Add<Bar> for Foo {
-    type Output = FooBar;
-    fn add(self, _rhs: Bar) -> Self::Output {
-        println!("> Bar.add(Foo) was Called");
-        FooBar
-    }
-}
-// The `std::opp::Add` trait is used to sepcify the functionaltiey of `+`
-// Here, we make `Add<Bar>` - the trait for addition with RHS of type `Foo`
-// This block implements teh opration: Bar + Foo = BarFOo
-
-impl ops::Add<Foo> for Bar {
-    type Output = BarFoo;
-    fn add(self, _rhs: Foo) -> Self::Output {
-        println!("> Bar.add(Foo) was Called");
-        BarFoo
-    }
-}
-
 fn main() {
-    println!("Foo + Bar = {:?}", Foo + Bar);
-    println!("Bar + Foo = {:?}", Bar + Foo);
-    println!("Hello, world!");
+    let string1: String = String::from("Long string is long");
+    let result;
+    {
+        let string2 = String::from("xyz");
+        result = longest(string1.as_str(), string2.as_str());
+    }
+    println!("The longest string is {result}")
+}
+
+fn longest<'a>(str_x: &'a str, str_y: &'a str) -> &'a str {
+    if str_x.len() > str_y.len() {
+        str_x
+    } else {
+        str_y
+    }
 }
